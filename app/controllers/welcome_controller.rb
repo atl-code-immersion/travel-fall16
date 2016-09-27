bb2ca757acef22b432d64a5a17c85eb0a7e50243
Aaron Groch <ar.groch@gmail.com>
2016-09-27 10:21:29 -0400

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,25 @@
+class WelcomeController < ApplicationController
+  def index
+  	@homestate = 'California'
+  	@countries = ['Kyrgyzstan','Bolivia','Central African Republic']
+
+  	@images = ['poppies.jpg','kyrgyzstan1.jpg','yurt-mcgurt.jpg','whos-gonna-ride-your-wild-horses.jpg']
+
+  	@picture_hash = {'poppies.jpg' => 'Poppies', 'yurt-mcgurt.jpg' => 'Yurt Living'}
+  end
+
+  def about
+  	@color = params[:color]
+  	@size = params[:size].to_i
+  end
+end
+
+
+
+
+
+
+
+
+
+
